ff09cf273465ecb472bcd4cad492f33bcc349a2f
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-11-08 20:23:16 +0100

    feat: Create export component Card

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+export default function Card ({ title, body, image, style, children, ...rest }) {
+  const styles = {
+    color: "black",
+    backgroundColor: "transparent",
+    padding: "10px",
+    border: "1px solid #eaeef3",
+    borderRadius: "10px",
+    ...style
+  };
+  return (
+    <div style={styles}>
+      { title ? <h1>{ title }</h1> : null }
+      { body ? <p>{ body }</p> : null }
+      { image ? <img src={image} alt="Image" /> : null }
+      { children }
+    </div>
+  );
+}
